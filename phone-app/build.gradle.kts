fba2c97b1f09455573ab9fe3b6b3b46d64d5fb5a
Anezium <anezium@gmail.com>
2026-05-26 19:17:52 +0200

    Fix Android phone splash screen

--- phone-app/build.gradle.kts
+++ phone-app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.anezium.rokidlive.phone"
         minSdk = 30
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
 
     compileOptions {
